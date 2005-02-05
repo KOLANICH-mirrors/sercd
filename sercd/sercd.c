b502a98d017888d95812c3d5a17bdfa59d93a5bc
Peter Astrand <astrand@lysator.liu.se>
2005-02-05 16:14:31 +0000

    Updated copyright header; it's 2005 now.

--- sercd/sercd.c
+++ sercd/sercd.c
@@ -1,6 +1,6 @@
 /*
     sercd: RFC 2217 compliant serial port redirector
-    Copyright (C) 2003 Cendio AB
+    Copyright (C) 2003 - 2005 Cendio AB
     Copyright (C) 1999 - 2003 InfoTecna s.r.l.
     Copyright (C) 2001, 2002 Trustees of Columbia University
     in the City of New York
