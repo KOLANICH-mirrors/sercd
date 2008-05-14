904d98cdf7f350f97be68a783b10c3128af9105b
Peter Astrand <astrand@lysator.liu.se>
2008-05-14 09:31:42 +0000

    Moved GetModemState and SetPort* to unix.c. LogMsg is now defined in sercd.h.

--- sercd.h
+++ sercd.h
@@ -7,4 +7,8 @@ typedef enum
 { False, True }
 Boolean;
 
+/* Generic log function with log level control. Uses the same log levels
+of the syslog(3) system call */
+void LogMsg(int LogLevel, const char *const Msg);
+
 #endif /* SERCD_H */
