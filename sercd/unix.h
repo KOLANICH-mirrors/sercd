6b36c8f0f5391bbd909b35415e98c1a2770da8d2
Peter Astrand <astrand@lysator.liu.se>
2008-05-14 08:39:24 +0000

    First step against windows support. Introduced UNIX and Windows modules. The device handle has been generalized into the PORTHANDLE define.

---
+++ sercd/unix.h
@@ -0,0 +1,14 @@
+/*
+ * sercd UNIX support
+ * Copyright 2008 Peter Åstrand <astrand@cendio.se> for Cendio AB
+ * see file COPYING for license details
+ */
+
+#ifndef WIN32
+#ifndef SERCD_UNIX_H
+#define SERCD_UNIX_H
+
+#define PORTHANDLE int
+
+#endif /* SERCD_UNIX_H */
+#endif /* WIN32 */
---
+++ sercd/win.h
@@ -0,0 +1,15 @@
+/*
+ * sercd Windows support
+ * Copyright 2008 Peter Åstrand <astrand@cendio.se> for Cendio AB
+ * see file COPYING for license details
+ */
+
+#ifdef WIN32
+#ifndef SERCD_WIN_H
+#define SERCD_WIN_H
+#include <windows.h>
+
+#define PORTHANDLE HANDLE
+
+#endif /* SERCD_WIN_H */
+#endif /* WIN32 */
