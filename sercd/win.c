03ce3a7be98941019a13d6a2192e54ff58c954c3
Peter Astrand <astrand@lysator.liu.se>
2008-05-26 08:39:21 +0000

    Implemented win32 version of LogMsg.

--- sercd/win.c
+++ sercd/win.c
@@ -7,5 +7,18 @@
 #ifdef WIN32
 #include "win.h"
 
+#include <stdio.h>
+
+extern int MaxLogLevel;
+
+/* Some day, we might want to support logging to Windows event log */
+void
+LogMsg(int LogLevel, const char *const Msg)
+{
+    if (LogLevel <= MaxLogLevel) {
+	fprintf(stderr, "%s\n", Msg);
+    }
+}
+
 
 #endif /* WIN32 */
