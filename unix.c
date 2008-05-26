2cf462b144b84e27056ab9e4cab465b7ff34106b
Peter Astrand <astrand@lysator.liu.se>
2008-05-26 05:49:14 +0000

    Removed message about "device in use" upon open error. Might not be true.

--- unix.c
+++ unix.c
@@ -663,9 +663,6 @@ OpenPort(const char *DeviceName, const char *LockFileName, PORTHANDLE * PortFd)
     /* Open the device */
     if ((*PortFd = open(DeviceName, O_RDWR | O_NOCTTY | O_NDELAY, 0)) == OpenError) {
 	/* Open failed */
-	snprintf(LogStr, sizeof(LogStr), "Device in use. Come back later.\r\n");
-	LogStr[sizeof(LogStr) - 1] = '\0';
-	LogMsg(LOG_ERR, LogStr);
 	snprintf(LogStr, sizeof(LogStr), "Unable to open device %s. Exiting.", DeviceName);
 	LogStr[sizeof(LogStr) - 1] = '\0';
 	LogMsg(LOG_ERR, LogStr);
