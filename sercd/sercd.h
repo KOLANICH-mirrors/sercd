29f62d9fb36413100eaa617733a7c526bda216af
Peter Astrand <astrand@lysator.liu.se>
2008-06-09 15:43:37 +0000

    Abstracted select loop:
    * The MIN/MAX macros are now defined in sercd.h, to be usable in other modules as well.
    
    * DropConnection is implemented in the platform modules. It is also
    called from ExitFunction.
    
    * The new platform function NewListener() is called when we have
    started listening on our socket.
    
    * The new abstract SercdSelect has been implemented. The UNIX module
    implements this one by using a straight-forward select loop. The
    function returns bits indicating what kind of events occured.
    
    * Reading/writing to the device and sockets are now done using the
    abstract ReadFromDev/WriteToDev/WriteToNet/ReadFromNet.
    
    * We are closing sockets with "closesocket" rather than "close".
    
    * When using standalone mode, OutSocketFd and InSocketFd points to the
    same integer, to allow checking for this case.
    
    * In the select loop, we are continuing after IO errors, since socket
    and device fds might not be valid any longer.

--- sercd/sercd.h
+++ sercd/sercd.h
@@ -2,6 +2,10 @@
 #ifndef SERCD_H
 #define SERCD_H
 
+#include "unix.h"
+#include "win.h"
+#include <sys/types.h>
+
 /* Standard boolean definition */
 typedef enum
 { False, True }
@@ -24,5 +28,32 @@ void ExitFunction(void);
 
 /* Function called on break signal */
 void BreakFunction(int unused);
+
+/* Abstract platform-independent select function */
+int SercdSelect(PORTHANDLE *DeviceIn, PORTHANDLE *DeviceOut,
+		SERCD_SOCKET *SocketOut, SERCD_SOCKET *SocketIn,
+		SERCD_SOCKET *SocketConnect, long PollInterval);
+#define SERCD_EV_DEVICEIN 1
+#define SERCD_EV_DEVICEOUT 2
+#define SERCD_EV_SOCKETOUT 4
+#define SERCD_EV_SOCKETIN 8
+#define SERCD_EV_SOCKETCONNECT 16
+
+/* macros */
+#ifndef MAX
+#define MAX(x,y)                (((x) > (y)) ? (x) : (y))
+#endif
+#ifndef MIN
+#define MIN(x,y)                (((x) > (y)) ? (y) : (x))
+#endif
+
+void NewListener(SERCD_SOCKET LSocketFd);
+void DropConnection(PORTHANDLE * DeviceFd, SERCD_SOCKET * InSocketFd, SERCD_SOCKET * OutSocketFd, 
+		    const char *LockFileName);
+
+ssize_t WriteToDev(PORTHANDLE port, const void *buf, size_t count);
+ssize_t ReadFromDev(PORTHANDLE port, void *buf, size_t count);
+ssize_t WriteToNet(SERCD_SOCKET sock, const void *buf, size_t count);
+ssize_t ReadFromNet(SERCD_SOCKET sock,  void *buf, size_t count);
 
 #endif /* SERCD_H */
